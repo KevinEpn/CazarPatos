d84fbb99712b2347685c5cf85f62d941cd985ae5
P3E004-K <P3E004-K@epn.edu.ec>
2025-05-16 15:10:46 -0500

    Added login activity

--- app/src/main/java/com/kevin/asimbaya/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/kevin/asimbaya/cazarpatos/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kevin.asimbaya.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        buttonLogin.setOnClickListener {
+            // Navegar a MainActivity
+            val intent = Intent(this, HelloActivity::class.java)
+            startActivity(intent)
         }
     }
 }
